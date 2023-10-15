f58e1fcc28c3d0e5a302e0b5474c8c1ae80c1984
ismayilras@code.edu.az <ismayilras@code.edu.az>
2023-10-15 23:51:52 +0400

    I create productCard

--- Final Project/assets/js/home.js
+++ Final Project/assets/js/home.js
@@ -1,13 +1,79 @@
+$(document).ready(function() {
+ 
+  $("#owl-demo").owlCarousel({
+ 
+      navigation : true, // Show next and prev buttons
+ 
+      slideSpeed : 2300,
+      paginationSpeed : 2300,
+ 
+      items : 1, 
+      itemsDesktop : false,
+      itemsDesktopSmall : false,
+      loop:true,
+      itemsTablet: false,
+      itemsMobile : false,
+      autoplay:true,
+      autoplayTimeout:2500,
+      autoplayHoverPause:true
 
-$(function () {
-    $('.mySwiper').slick({
-      dots: true,
-      infinite: true,
-      speed: 300,
-      slidesToShow: 1,
-      adaptiveHeight: true
-    })
+  });
+ 
 });
 
+// $('.product_cards').owlCarousel({
+//   loop:true,
+//   margin:10,
+//   responsiveClass:true,
+//   responsive:{
+//       0:{
+//           items:1,
+//           nav:true
+//       },
+//       600:{
+//           items:3,
+//           nav:false
+//       },
+//       1000:{
+//           items:5,
+//           nav:true,
+//           loop:false
+//       }
+//   }
+// })
 
-  
+// $('.product_cards').slick({
+//   dots: true,
+//   infinite: false,
+//   speed: 300,
+//   slidesToShow: 4,
+//   slidesToScroll: 4,
+//   responsive: [
+//     {
+//       breakpoint: 1024,
+//       settings: {
+//         slidesToShow: 2,
+//         slidesToScroll: 2,
+//         infinite: true,
+//         dots: true
+//       }
+//     },
+//     {
+//       breakpoint: 600,
+//       settings: {
+//         slidesToShow: 1,
+//         slidesToScroll: 1
+//       }
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         slidesToShow: 1,
+//         slidesToScroll: 1
+//       }
+//     }
+//     // You can unslick at a given breakpoint now by adding:
+//     // settings: "unslick"
+//     // instead of a settings object
+//   ]
+// });
