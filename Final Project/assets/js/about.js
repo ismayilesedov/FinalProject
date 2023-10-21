f776eef3469ccda364d73e062049b200017fb0b5
ismayilras@code.edu.az <ismayilras@code.edu.az>
2023-10-21 16:35:18 +0400

    I create about page

---
+++ Final Project/assets/js/about.js
@@ -0,0 +1,22 @@
+// $(document).ready(function() {
+ 
+//     $("").owlCarousel({
+   
+//         navigation : true, // Show next and prev buttons
+   
+//         slideSpeed : 2300,
+//         paginationSpeed : 2300,
+   
+//         items : 1, 
+//         itemsDesktop : false,
+//         itemsDesktopSmall : false,
+//         loop:true,
+//         itemsTablet: false,
+//         itemsMobile : false,
+//         autoplay:true,
+//         autoplayTimeout:2500,
+//         autoplayHoverPause:true
+  
+//     });
+   
+//   });
